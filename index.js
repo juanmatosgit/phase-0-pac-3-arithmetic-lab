df658be786137f4fd0bcecb284f50b1cd0141c94
Juan <ssjmatos@gmail.com>
2021-11-28 16:17:18 -0500

    lab arithmetic complete Sunday

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    2 + 2
+}
+
+function subtract() {
+    12 - 2
+}
+
+function multiply() {
+    2 * 2
+}
+
+function divide() {
+    3 / 3
+}
+
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function increment(n) {
+    n++
+    return n
+}
+
+function decrement(n) {
+    n--
+    return n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+   
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
